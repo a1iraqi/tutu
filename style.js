7eee48b8314e5a90e0145043639b03c4ed60618c
a1iraqi <abood.fi123@gmail.com>
2017-09-17 22:14:46 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,61 @@
+/*  16:9 Ratio Container*/
+
+.img-responsive {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  max-height: 300px;
+}
+
+.icon {
+  border-radius: 25%;
+  background-color: #fff;
+  max-width: 128px;
+}
+
+.homeapp-title {
+  color:#000;
+}
+
+.small-banner {
+  background-color: #c3c3c3;
+  border-radius: 25px;
+  display: block;
+  height: auto;
+  max-width: 256px;
+}
+
+.screenshot {
+  max-width: 375px;
+}
+
+.library-item,
+.library-item a {
+  color:#ffffff;
+}
+.button-library {
+  border: 1px solid #fff;
+  color: #007aff;
+  text-decoration: none;
+  text-align: center;
+  display: block;
+  border-radius: 5px;
+  line-height: 27px;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -ms-appearance: none;
+  appearance: none;
+  background: none;
+  padding: 0 10px;
+  margin: 0;
+  height: 29px;
+  white-space: nowrap;
+  position: relative;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 14px;
+  font-family: inherit;
+  cursor: pointer;
+  outline: 0;
+}
